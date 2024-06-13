c47849ffa27d264fe32849e298885daf28199184
Andres Hung <andres.hung@outlook.com>
2024-06-13 10:36:07 -0600

    Shorten install process

--- install.sh
+++ install.sh
@@ -6,67 +6,30 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# prompt for complete or minimum installation for brewfile
-read -p "Complete or minimal installation? [C/m]: " choice
-
-# make the choice case-insensitive
-choice=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
+# run the brew bundle command to install minimum packages
 
 # detect linux or macOS
 os_name=$(uname -s)
 
-# run the brew bundle command to install applications
-
 # on linux systems
 if [ "$os_name" == "Linux" ]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-  if [[ "$choice" == 'c' ]]; then
-    brew bundle --file ./etc/packages/homebrew/linux-backup
-  else
-    brew bundle --file ./etc/packages/homebrew/linux-brewfile-min
-  fi
+  brew bundle --file ./etc/packages/homebrew/linux-minimum
 fi
 
 # on macOS systems
 if [ "$os_name" == "Darwin" ]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
-
-  if [[ "$choice" == 'c' ]]; then
-    brew bundle --file ./etc/packages/homebrew/macos-brewfile
-  else
-    brew bundle --file ./homebrew/macos-brewfile-min
-  fi
+  brew bundle --file ./etc/packages/homebrew/macos-minimum
 fi
 
 # run git stow
+stow --adopt */ && git restore .
 
-read -p "Run stow on all packages? [Y/n]: " choice
-
-# make the choice case-insensitive
-choice=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
-
-# on linux systems
-if [ "$os_name" == "Linux" ]; then
-  if [[ "$choice" == 'y' || "$choice" == '' ]]; then
-    stow --adopt */ && git restore .
-  fi
-fi
-
-# on macOS systems
-if [ "$os_name" == "Darwin" ]; then
-  if [[ "$choice" == 'y' || "$choice" == '' ]]; then
-    stow --adopt */ && git restore .
-  fi
-fi
-
-# prompt for wakatime API key
+# wakatime setup
 read -p "Enter wakatime API key: " wakatime_api_key
-
-# create wakatime.cfg file
 echo "[settings]" > ~/.wakatime.cfg
 echo "api_key=$wakatime_api_key" >> ~./wakatime.cfg
-
 # link wakatime-cli to homebrew package for zsh
 mkdir ~/.wakatime && ln -s $(which wakatime-cli) ~/.wakatime/wakatime-cli 
 
