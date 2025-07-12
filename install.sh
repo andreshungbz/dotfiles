085b4164adf71f6fc61b1852ea21b822755ca6bb
Andres Hung <andres.hung@outlook.com>
2025-07-12 14:04:18 -0600

    Add macOS consideration

--- install.sh
+++ install.sh
@@ -4,8 +4,20 @@ set -e
 # Homebrew Package Manager
 # https://brew.sh/
 
+# determine macOS/Linux system
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  BREW_PATH="/opt/homebrew/bin/brew"
+else
+  BREW_PATH="/home/linuxbrew/.linuxbrew/bin/brew"
+fi
+
+# install non-interactively
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# add to PATH and evaluate
+echo "eval \"\$($BREW_PATH shellenv)\"" >> ~/.bashrc
+echo "eval \"\$($BREW_PATH shellenv)\"" >> ~/.zshrc
+eval "$($BREW_PATH shellenv)"
+
+# install packages from list
 brew bundle --file ./packages/Brewfile
